8a6ecafffa0cf43d61e056cb4a709ec9937e4569
MICSTI <stifter.michael@gmx.net>
2016-09-20 08:59:05 +0200

    Initial setup

---
+++ index.js
@@ -0,0 +1,26 @@
+// module imports
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// config
+var port = process.env.PORT || 9000;
+
+// app instance
+var app = express();
+
+// get all data of the body (POST) parameters
+app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// start app
+app.listen(port);
+
+console.log('Plain WebRTC started on port', port);
+
+// expose app
+exports = module.exports = app;
